2bc0d1863c1d76d9262b89eefc72f32f166548c9
zbagdzevicius <zbagdzevicius@gmail.com>
2019-05-20 19:54:14 +0300

    A

--- resolution.py
+++ resolution.py
@@ -6,6 +6,7 @@ from time import time
 class Resolution:
     def __init__(self, picture_location):
         self.picture = Image.open(picture_location)
+        self.picture = self.picture.convert('RGB')
         self.picture_width, self.picture_height = self.picture.size
 
     def change_resolution(self, how_many_times_to_enlarge):
@@ -16,36 +17,28 @@ class Resolution:
 
         changed_picture = self.__make_empty_picture(
             (new_picture_width, new_picture_height))
-        surounding_pixels = [(0, 0) for value in range(9)]
-        for x in range(new_picture_width):
-            for y in range(new_picture_height):
-                try:
-                    picture_coordinate_x = round(x/how_many_times_to_enlarge)
-                    picture_coordinate_y = round(y/how_many_times_to_enlarge)
 
-                    surounding_pixels[0] = self.picture.getpixel(
-                        (picture_coordinate_x - 1, picture_coordinate_y - 1))
-                    surounding_pixels[1] = self.picture.getpixel(
-                        (picture_coordinate_x - 1, picture_coordinate_y))
-                    surounding_pixels[2] = self.picture.getpixel(
-                        (picture_coordinate_x - 1, picture_coordinate_y + 1))
-                    surounding_pixels[3] = self.picture.getpixel(
-                        (picture_coordinate_x,   picture_coordinate_y - 1))
-                    surounding_pixels[4] = self.picture.getpixel(
-                        (picture_coordinate_x, picture_coordinate_y))
-                    surounding_pixels[5] = self.picture.getpixel(
-                        (picture_coordinate_x, picture_coordinate_y + 1))
-                    surounding_pixels[6] = self.picture.getpixel(
-                        (picture_coordinate_x + 1, picture_coordinate_y - 1))
-                    surounding_pixels[7] = self.picture.getpixel(
-                        (picture_coordinate_x + 1, picture_coordinate_y))
-                    surounding_pixels[8] = self.picture.getpixel(
-                        (picture_coordinate_x + 1, picture_coordinate_y + 1))
 
-                    median_pixel = self.__calculate_median_pixel(surounding_pixels)
-                    changed_picture.putpixel((x, y), median_pixel)
-                except:
-                    pass
+        surounding_pixels_array = [(0, 0) for value in range(9)]
+        pixels_multidimensional_array = self.picture.load()
+        pixels_to_remove_for_simplicity_of_algorithm = 2 * how_many_times_to_enlarge
+
+        for x in range(pixels_to_remove_for_simplicity_of_algorithm, new_picture_width - pixels_to_remove_for_simplicity_of_algorithm):
+            for y in range(pixels_to_remove_for_simplicity_of_algorithm, new_picture_height-pixels_to_remove_for_simplicity_of_algorithm):
+                picture_coordinate_x = round(x/how_many_times_to_enlarge)
+                picture_coordinate_y = round(y/how_many_times_to_enlarge)
+
+                surounding_pixels_array[0] = pixels_multidimensional_array[picture_coordinate_x - 1,picture_coordinate_y - 1]
+                surounding_pixels_array[1] = pixels_multidimensional_array[picture_coordinate_x - 1,picture_coordinate_y]
+                surounding_pixels_array[2] = pixels_multidimensional_array[picture_coordinate_x - 1,picture_coordinate_y + 1]
+                surounding_pixels_array[3] = pixels_multidimensional_array[picture_coordinate_x,picture_coordinate_y - 1]
+                surounding_pixels_array[4] = pixels_multidimensional_array[picture_coordinate_x,picture_coordinate_y]
+                surounding_pixels_array[5] = pixels_multidimensional_array[picture_coordinate_x,picture_coordinate_y + 1]
+                surounding_pixels_array[6] = pixels_multidimensional_array[picture_coordinate_x + 1,picture_coordinate_y - 1]
+                surounding_pixels_array[7] = pixels_multidimensional_array[picture_coordinate_x + 1,picture_coordinate_y]
+                surounding_pixels_array[8] = pixels_multidimensional_array[picture_coordinate_x + 1,picture_coordinate_y + 1]
+                median_pixel = self.__calculate_median_pixel(surounding_pixels_array)
+                changed_picture.putpixel((x, y), median_pixel)
 
         return changed_picture
 
